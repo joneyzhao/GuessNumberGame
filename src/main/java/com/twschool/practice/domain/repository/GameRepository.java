3f14c222d77c930ae0c7c13f6b12e9d2e3922cc5
zhaohuanhuan <1297972106@qq.com>
2020-06-22 21:12:16 +0800

    新增repository找到已有Game的方法

--- src/main/java/com/twschool/practice/domain/repository/GameRepository.java
+++ src/main/java/com/twschool/practice/domain/repository/GameRepository.java
@@ -4,7 +4,14 @@ import com.twschool.practice.domain.AnswerGenerator;
 import com.twschool.practice.domain.GuessNumberGame;
 
 public class GameRepository {
+    private GuessNumberGame guessNumberGame;
+
     public GuessNumberGame create() {
-        return new GuessNumberGame(new AnswerGenerator());
+        GuessNumberGame guessNumberGame = new GuessNumberGame(new AnswerGenerator());
+        return guessNumberGame;
+    }
+
+    public GuessNumberGame find() {
+        return guessNumberGame;
     }
 }
