d6c1ee4b294bd1eed004b948ad1d2fdfa999c21a
zhaohuanhuan <1297972106@qq.com>
2020-06-22 16:16:07 +0800

    完成guessController接口测试-第一步目录

--- src/test/java/com/twschool/practice/domain/api/GuessControllerTest.java
+++ src/test/java/com/twschool/practice/domain/api/GuessControllerTest.java
@@ -1,4 +1,4 @@
-package api;
+package com.twschool.practice.domain.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@ContextConfiguration
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 public class GuessControllerTest {
