a88632f5f696c8fa339b89f06d2b27980d4eaf4a
zhaohuanhuan <1297972106@qq.com>
2020-06-23 20:54:43 +0800

    两个用户玩游戏玩游戏多次测试通过

--- src/test/java/com/twschool/practice/domain/UserTest.java
+++ src/test/java/com/twschool/practice/domain/UserTest.java
@@ -26,6 +26,28 @@ public class UserTest {
 
         Assert.assertEquals("3A0B", resultTwo);
         Assert.assertEquals(0, userTwo.getTotalGameScores());
+    }
 
+    @Test
+    public void should_return_game_result_when_create_two_user_and_one_of_them_continus_success_6_times(){
+        User userOne = new User("12");
+        User userTwo = new User("11");
+
+        userOne.getResultString("1 2 3 4");
+        userOne.getResultString("1 2 3 4");
+        userOne.getResultString("1 2 3 4");
+        userOne.getResultString("1 2 3 4");
+        userOne.getResultString("1 2 3 4");
+        userOne.getResultString("1 2 3 4");
+
+        userTwo.getResultString("1 5 3 4");
+        userTwo.getResultString("1 5 3 4");
+        userTwo.getResultString("1 5 3 4");
+        userTwo.getResultString("1 5 3 4");
+        userTwo.getResultString("1 5 3 4");
+        userTwo.getResultString("1 5 3 4");
+
+        Assert.assertEquals(25, userOne.getTotalGameScores());
+        Assert.assertEquals(-3, userTwo.getTotalGameScores());
     }
 }
